4a7c8fcb186f5e8d8edd94e2356be690132138f6
Pavel <shegol164@mail.ru>
2025-04-01 14:20:55 +0400

    Add user.py

---
+++ src/OOP_14_1/lesson_2/user.py
@@ -0,0 +1,49 @@
+"""
+Напишите класс User, имеющий следующие свойства и методы:
+
+- __init__(self, name, password): конструктор, принимающий имя пользователя и пароль
+- name: свойство, которое возвращает имя пользователя
+- password: свойство, которое позволяет установить или изменить пароль пользователя
+- is_admin: свойство, которое возвращает, является ли пользователь администратором или нет
+- _is_admin: свойство-помощник, которое определяет, является ли пользователь администратором или нет
+- login(self, password): метод, который проверяет, соответствует ли введенный пароль паролю пользователя
+- logout(self): метод, который выходит из аккаунта пользователя (устанавливает значение свойства _is_logged_in в False при условии, что пользователь залогинен)
+
+Для свойств name и password используйте декораторы @property и @password.setter.
+"""
+
+
+class User:
+    def __init__(self, name, password):
+        self._name = name
+        self._password = password
+        self._is_admin = False
+        self._is_logged_in = False
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, new_password):
+        self._password = new_password
+
+    @property
+    def is_admin(self):
+        return self._is_admin
+
+    def login(self, password):
+        if password == self._password:
+            self._is_logged_in = True
+            return True
+        return False
+
+    def logout(self):
+        if self._is_logged_in:
+            self._is_logged_in = False
+
+# код для проверки
